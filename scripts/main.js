853a387e817de6bc95421eea88b10dbaf2169dc9
Maxym Dyachenko <Maksym.Diachenko@infopulse.com>
2017-10-21 18:38:20 +0300

    ***

---
+++ scripts/main.js
@@ -0,0 +1,34 @@
+$( document ).ready(function() {
+    //menu dropdown animation
+    $('.toggler').click(function () {
+            $(this).toggleClass('active');
+            $('.dropdown').slideToggle(250);
+        }
+    );
+    //scroll for aside menu
+    $(function(){
+        $('#content').slimScroll({
+            height: 'auto',
+            color: '#78c8d6'
+        });
+    });
+    $('.mobile-menu').click(function () {
+        $('aside').toggleClass('active');
+        $('body').toggleClass('scroll-hidden');
+    });
+
+    //scroll to top from bottom
+    var $root = $('html, body');
+
+    $('#go-to-top').click(function () {
+        $root.animate({
+            scrollTop: $('#section1').offset().top
+        }, 500);
+
+        return false;
+    });
+
+    $('.content-desc').each(function(index, element) {
+        $clamp(element, { clamp: 3, useNativeClamp: false });
+    });
+});
