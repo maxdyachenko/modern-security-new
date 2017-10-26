3f7da61645a84314ba05731f535d148b083e7194
Maxym Dyachenko <max.dyachenko@inbox.ru>
2017-10-26 18:04:21 +0300

    add sort logic

--- wp-content/themes/twentyseventeen/ждёт/scripts/main.js
+++ wp-content/themes/twentyseventeen/ждёт/scripts/main.js
@@ -54,40 +54,20 @@ $( document ).ready(function() {
 
     })
 
-    //slider
-    $('.responsive').slick({
-        dots: true,
-        infinite: false,
-        speed: 300,
-        slidesToShow: 4,
-        slidesToScroll: 4,
-        responsive: [
-            {
-                breakpoint: 1024,
-                settings: {
-                    slidesToShow: 3,
-                    slidesToScroll: 3,
-                    infinite: true,
-                    dots: true
-                }
-            },
-            {
-                breakpoint: 600,
-                settings: {
-                    slidesToShow: 2,
-                    slidesToScroll: 2
-                }
-            },
-            {
-                breakpoint: 480,
-                settings: {
-                    slidesToShow: 1,
-                    slidesToScroll: 1
-                }
-            }
-            // You can unslick at a given breakpoint now by adding:
-            // settings: "unslick"
-            // instead of a settings object
-        ]
-    });
+    //result of work sort
+    var data = $('.documents-container').children(),
+        swapped = false,
+        date = null, date1 = null;
+    for (var i = 0; i < data.length - 1; i++){
+        date = $(data[i]).find($('.date')).html();
+        date1 = $(data[i+1]).find($('.date')).html();
+        debugger;
+        if (date > date1) {
+            var temp = data[i];
+            data[i] = data[i+1];
+            data[i+1] = temp;
+            swapped = true;
+        }
+    }
+
 });
